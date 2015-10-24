9e1f16bee39a3278ee2d0db7d9c3c7cfe7bd319e
Mike Pountney <mikepea@yelp.com>
2015-10-24 11:57:54 -0700

    Add more useful work filters. Enough to start with now.

--- yelp/build.rb
+++ yelp/build.rb
@@ -4,10 +4,54 @@ require 'rubygems'
 require 'gmail-britta'
 
 fs = GmailBritta.filterset(:me => ['mikepea@yelp.com']) do
+
   filter {
     has %w{from:asf@boinkor.net}
     label 'from-the-author-of-gmail-britta'
     never_spam
   }
+
+  filter {
+    has %w{list:(<engineering.yelp.com>)}
+    label 'Eng'
+    never_spam
+  }
+
+  filter {
+    has %w{list:(<engineering.yelp.com>)}
+    label 'Lists/engineering'
+    never_spam
+  }
+
+  filter {
+    has %w{list:(<engineering.yelp.com>)}
+    label '__TeamInbox'
+    never_spam
+  }.archive_unless_directed
+
+  filter {
+    has %w{list:(<caskforce.yelp.com>)}
+    label '_Social'
+    never_spam
+  }.archive_unless_directed
+
+  filter {
+    has %w{list:(<boardgames.yelp.com>)}
+    label '_Social'
+    never_spam
+  }.archive_unless_directed
+
+  filter {
+    has %w{to:systems+awsspam@yelp.com}
+    label 'Systems/awsspam'
+    never_spam
+  }.archive_unless_directed
+
+  filter {
+    has %w{to:infra-changes@yelp.com}
+    label 'infra-changes'
+    never_spam
+  }
+
 end
 puts fs.generate
