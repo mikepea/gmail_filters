e1d395c704ab4cf1a2fc8987e683f6ba6eb86798
Mike Pountney <mikepea@yelp.com>
2015-12-07 15:49:29 +0000

    more filters

--- home/build.rb
+++ home/build.rb
@@ -4,10 +4,78 @@ require 'rubygems'
 require 'gmail-britta'
 
 fs = GmailBritta.filterset(:me => ['Mike.Pountney@gmail.com']) do
+
   filter {
     has %w{from:asf@boinkor.net}
     label 'from-the-author-of-gmail-britta'
     never_spam
   }
+
+  filter {
+    has %w{list:(<brightonhackerspace.googlegroups.com>)}
+    label 'Lists/brightonhackerspace'
+    never_spam
+  }
+
+  filter {
+    has %w{list:(<outages.outages.org>)}
+    label 'Lists/outages'
+    never_spam
+  }
+
+  filter {
+    has %w{list:(<nanog.nanog.org>)}
+    label 'Lists/NANOG'
+    never_spam
+  }.archive_unless_directed
+
+  filter {
+    has %w{list:(<london-hack-space.googlegroups.com>)}
+    label 'Lists/london-hack-space'
+    never_spam
+  }.archive_unless_directed
+
+  filter {
+    has %w{list:(<makerspaceca.googlegroups.com>)}
+    label 'Lists/makerspaceca'
+    never_spam
+  }.archive_unless_directed
+
+  filter {
+    has %w{list:(<power-racing-series.googlegroups.com>)}
+    label 'Lists/power-racing-series'
+    never_spam
+  }.archive_unless_directed
+
+  filter {
+    has %w{list:(<notifs.direct.ilovefreegle.org>)}
+    label 'Lists/GreenCycleSussex'
+    never_spam
+  }.archive_unless_directed
+
+  filter {
+    has %w{list:(<nottinghack.googlegroups.com>)}
+    label 'Lists/nottinghack'
+    never_spam
+  }.archive_unless_directed
+
+  filter {
+    has %w{list:(<myfordlathes.yahoogroups.com>)}
+    label 'Lists/myfordlathes'
+    never_spam
+  }.archive_unless_directed
+
+  filter {
+    has %w{list:(<hackspacebristol.lists.aktivix.org>)}
+    label 'Lists/hackspacebristol'
+    never_spam
+  }.archive_unless_directed
+
+  filter {
+    has %w{list:(<derby-makers.googlegroups.com>)}
+    label 'Lists/derby-makers'
+    never_spam
+  }.archive_unless_directed
+
 end
 puts fs.generate
--- yelp/build.rb
+++ yelp/build.rb
@@ -12,6 +12,66 @@ fs = GmailBritta.filterset(:me => ['mikepea@yelp.com']) do
   }
 
   filter {
+    has %w{from:operations+diskquota@}
+    label 'Systems/other-folks-diskquota'
+    never_spam
+  }.archive_unless_directed
+
+  filter {
+    has %w{list:(<inframan.yelp.com>) subject:"Review Request"}
+    label '_INFRA/non-ops-reviewboards'
+    never_spam
+  }.archive_unless_directed
+
+  filter {
+    has %w{from:sensu@ list:(<inframan.yelp.com>)}
+    label 'Systems/inframan-alerts'
+    never_spam
+  }.archive_unless_directed
+
+  filter {
+    has %w{from:root@ list:(<inframan.yelp.com>)}
+    label 'Systems/inframan-rootspam'
+    never_spam
+  }.archive_unless_directed
+
+  filter {
+    has %w{to:root@ subject:fcron}
+    label 'Systems/cronjunk'
+    never_spam
+  }.archive_unless_directed
+
+  filter {
+    has %w{to:systems@ subject:Cron}
+    label 'Systems/cronjunk'
+    never_spam
+  }.archive_unless_directed
+
+  filter {
+    has %w{to:root@ subject:Cron}
+    label 'Systems/cronjunk'
+    never_spam
+  }.archive_unless_directed
+
+  filter {
+    has %w{list:(<operations.yelp.com>) to:root@}
+    label 'Systems/rootjunk'
+    never_spam
+  }.archive_unless_directed
+
+  filter {
+    has %w{to:batch+sfo2@yelp.com}
+    label 'Systems/tron/prod'
+    never_spam
+  }.archive_unless_directed
+
+  filter {
+    has %w{to:batch+stage@yelp.com}
+    label 'Systems/tron/stage'
+    never_spam
+  }.archive_unless_directed
+
+  filter {
     has %w{list:(<engineering.yelp.com>)}
     label 'Eng'
     never_spam
